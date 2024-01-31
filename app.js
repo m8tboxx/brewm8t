d5eeb4eb3b1cff7c3da0ddf54866480657e33cf6
Markus Langthaler <markus.langthaler@neoom.com>
2024-01-31 11:54:03 +0100

    First setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const mainRoutes = require('./routes/main');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(mainRoutes);
+
+app.listen(3000, () => {
+    console.info(`Server is running on port 3000...`);
+});
---
+++ models/receipt.js
@@ -0,0 +1,6 @@
+module.exports = class Receipt {
+    constructor(data) {
+        this.name = data.name;
+        this.mashTemp = data.mashTemp
+    }
+}
